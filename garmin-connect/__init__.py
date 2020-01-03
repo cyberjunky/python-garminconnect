ec480ae85280de7cafc6e2a6683c680f80f6127d
Ron Klinkien <ron@cyberjunky.nl>
2020-01-03 19:08:12 +0100

    Renamed module

--- garmin-connect/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Initialize the garmin-connect package."""
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="garmin-connect",
+    name="garmin_connect",
     version="0.1.0",
     author="Ron Klinkien",
     author_email="ron@cyberjunky.nl",
     description="Python API wrapper for Garmin Connect",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cyberjunky/garmin-connect",
+    url="https://github.com/cyberjunky/garmin_connect",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
