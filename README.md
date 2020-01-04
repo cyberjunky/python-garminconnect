a4d5389a4619bf8be2c3468f5ea72f2cbdec579d
Ron Klinkien <ron@cyberjunky.nl>
2020-01-04 20:32:40 +0100

    Added heartrates fetch code Code cleanup Renamed module

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
-# Garmin
-Python API wrapper for Garmin Connect
+# Python: Garmin Connect
 
+[![GitHub Release][releases-shield]][releases]
+[![License][license-shield]](LICENSE.md)
+
+Python 3 API wrapper for Garmin Connect to get your statistics.
+
+## About
+
+This package allows you to request your activity and health data you gather on Garmin Connect.
 See https://connect.garmin.com/
 
+
+## Installation
+
+```bash
+pip install garminconnect
+```
+
 ## Usage
-Create a new connection by supplying your user credentials
-```
-import garmin
 
-data = garmin.Garmin(YOUR_EMAIL, YOUR_PASSWORD)
-```
+```python
+import garminconnect
 
-Fetch your Garmin Connect activities data
-```
-print(data.fetch_stats('YYYY-mm-dd'))
-```
+"""Login to portal using specified credentials"""
+client = garminconnect.Garmin(YOUR_EMAIL, YOUR_PASSWORD)
 
+"""Fetch your activities data"""
+print(client.fetch_stats('2020-01-04'))
 
-Fetch your Garmin Connect heart rate data
-
-```
-print(data.fetch_heart_rates('YYYY-mm-dd'))
+"""Fetch your logged heart rates"""
+print(client.fetch_heart_rates('2020-01-04'))
 ```
--- setup.py
+++ setup.py
@@ -1,21 +1,46 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import re
+import sys
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name="garmin",
-    version="0.1.0",
+
+def get_version():
+    """Get current version from code."""
+    regex = r"__version__\s=\s\"(?P<version>[\d\.]+?)\""
+    path = ("garminconnect", "__version__.py")
+    return re.search(regex, read(*path)).group("version")
+
+
+def read(*parts):
+    """Read file."""
+    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts)
+    sys.stdout.write(filename)
+    with io.open(filename, encoding="utf-8", mode="rt") as fp:
+        return fp.read()
+
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
     author="Ron Klinkien",
     author_email="ron@cyberjunky.nl",
-    description="Python API wrapper for Garmin Connect",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cyberjunky/garmin",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    description="Python 3 API wrapper for Garmin Connect",
+    name="garminconnect",
+    keywords=["garmin connect", "api", "client"],
+    license="MIT license",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    url="https://github.com/cyberjunky/python-garminconnect",
+    packages=["garminconnect"],
+    version=get_version(),
 )
