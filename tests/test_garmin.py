cc87d35c4331f1f1720ee5d6e59f0ce1cef42bdd
Matin Tamizi <mtamizi@duck.com>
2023-08-17 22:03:21 -0600

    add more tests

--- tests/test_garmin.py
+++ tests/test_garmin.py
@@ -17,3 +17,34 @@ def test_stats(garmin):
     stats = garmin.get_stats(DATE)
     assert "totalKilocalories" in stats
     assert "activeKilocalories" in stats
+
+
+@pytest.mark.vcr
+def test_user_summary(garmin):
+    garmin.login()
+    user_summary = garmin.get_user_summary(DATE)
+    assert "totalKilocalories" in user_summary
+    assert "activeKilocalories" in user_summary
+
+
+@pytest.mark.vcr
+def test_steps_data(garmin):
+    garmin.login()
+    steps_data = garmin.get_steps_data(DATE)[0]
+    assert "steps" in steps_data
+
+
+@pytest.mark.vcr
+def test_floors(garmin):
+    garmin.login()
+    floors_data = garmin.get_floors(DATE)
+    assert "floorValuesArray" in floors_data
+
+
+@pytest.mark.vcr
+def test_daily_steps(garmin):
+    garmin.login()
+    daily_steps = garmin.get_daily_steps(DATE, DATE)[0]
+    assert "calendarDate" in daily_steps
+    assert "totalSteps" in daily_steps
+    assert "stepGoal" in daily_steps
